2fe13737c5cf6b88fd923bb393e311165956b1a7
spongah <rocksolid801@gmail.com>
2017-06-19 16:02:59 -0700

    add angular app via cli

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 var express = require('express');
+const path = require('path');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/angular-src/dist'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/angular-src/dist/index.html'));
 });
 
 app.listen(app.get('port'), function() {
