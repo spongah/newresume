83080c26fdd52b26e5fe87a67c2ff09571e136dd
spongah <rocksolid801@gmail.com>
2017-06-19 16:05:56 -0700

    testing

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/angular-src/dist'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/angular-src/dist/index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.listen(app.get('port'), function() {
