48090e1919b86a6ca92748570c2ee35b384ca119
spongah <rocksolid801@gmail.com>
2017-06-20 10:06:23 -0700

    adjust express get routes

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
